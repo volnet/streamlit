d051782bc53340bac4b397bf5ccf9ffedc28ab6b
volnet <volnet@qq.com>
2023-04-08 02:03:05 +0800

    fix bug

--- pages/7_openaikeydetect.py
+++ pages/7_openaikeydetect.py
@@ -67,7 +67,7 @@ if st.button("Detect"):
 
             if(keys_length > 0):
                 is_available, model_count, openai_return, error = check_key(key)
-                openai_return = openai_return.replace("|", "")
+                openai_return = openai_return.replace("|", "&#124;")
                 result += f"| {key} |  {is_available}  |  {model_count}  |  {openai_return}  |  {error}  |\n"
                 if is_available:
                     result_available += f"{key}<br />"
